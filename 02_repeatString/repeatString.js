51046af14a74947a1496f3890b99e3cb8985610d
Skubbed <167587317+Skubbed@users.noreply.github.com>
2024-07-19 18:21:55 -0700

    Finished ex. 1 & 2.

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,15 @@
-const repeatString = function() {
+const repeatString = function(string, count) {
+    if(count < 0){
+        return("ERROR");
+    }
+    let returnString = "";
+    
+    while(count > 0){
+        returnString += string;
+        count--;
+    }
 
+    return returnString;    
 };
 
 // Do not edit below this line
