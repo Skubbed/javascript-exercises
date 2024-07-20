9f27279edd32c958977c5a9ab63ce6b247c0c4e3
Skubbed <167587317+Skubbed@users.noreply.github.com>
2024-07-19 20:14:11 -0700

    Finished up to ex. 4

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,14 @@
-const reverseString = function() {
+const reverseString = function(string) {
+    let revString="", index = string.length-1;
 
+    while(index >= 0){        
+        revString += string[index];
+        
+        console.log(revString);
+        index--;
+    }
+
+    return revString;
 };
 
 // Do not edit below this line
--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,23 @@
 const removeFromArray = function() {
+    let len = arguments.length-1, index = 0;
+    if(len < 0){
+        return "ERROR";
+    }
+    while(len >= 1){
+        for(; index <= arguments[0].length-1; index++){
+            if(arguments[0][index] === arguments[len]){
+                arguments[0].splice(index, 1);
+                index--;
+            }
+        }
+        index = 0;
+        len--;
+    }
+    return arguments[0];
 };
 
 // Do not edit below this line
 module.exports = removeFromArray;
+
+
+
