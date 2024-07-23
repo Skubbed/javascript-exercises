626b88c4333966febda7e4e78cb1c1ddcac9cb86
Skubbed <167587317+Skubbed@users.noreply.github.com>
2024-07-23 13:33:38 -0700

    finished 08 calculator

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,39 @@
-const add = function() {
-	
+const add = function(a, b) {
+	return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+	return a - b;
 };
 
-const sum = function() {
-	
+const sum = function(arr) {
+	let sum = 0;
+  for(i = arr.length-1; i >= 0; i--){
+    sum += arr[i];
+  }
+  return sum;
 };
 
-const multiply = function() {
-
+const multiply = function(arr) {
+  let total = 1;
+  for(i = arr.length-1; i >= 0; i--){
+    total *= arr[i];
+  }
+  return total;
 };
 
-const power = function() {
-	
+const power = function(num, pow) {
+	return Math.pow(num, pow);
 };
 
-const factorial = function() {
-	
+const factorial = function(num) {
+	if(num < 1){return 1;}
+  let total = 1;
+  while(num > 0){
+    total *= num;
+    num--;
+  }
+  return total;
 };
 
 // Do not edit below this line
@@ -31,3 +45,6 @@ module.exports = {
   power,
   factorial
 };
+
+const arra = [1,2,3,5,0];
+console.log(sum(arra));
