a78c2930dcfb86237decc99c3f3bf7f31c575960
Skubbed <167587317+Skubbed@users.noreply.github.com>
2024-07-22 20:55:26 -0700

    Finished 07-tempConversion

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,12 @@
-const convertToCelsius = function() {
+//C = 5/9(F-32).
+const convertToCelsius = function(temp) {
+  return Math.round((((5/9) * (temp -32)).toFixed(1)) * 10) / 10;    
 };
 
-const convertToFahrenheit = function() {
+//F = (9/5 * C) + 32
+const convertToFahrenheit = function(temp) {
+  return Math.round((((9/5) * temp) + 32) * 10) / 10;
+
 };
 
 // Do not edit below this line
