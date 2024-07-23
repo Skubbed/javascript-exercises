866c7d39365557e43dc276b7000bc8571157c87b
Skubbed <167587317+Skubbed@users.noreply.github.com>
2024-07-22 20:38:59 -0700

    Finished 06 leapYears

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,6 +1,18 @@
-const leapYears = function() {
-
+const leapYears = function(year) {
+    if(year % 4 === 0){
+        if(year % 100 === 0){
+            if(year % 400 === 0){
+                return true;
+            }
+            else{
+                return false;
+            }
+        }
+        return true;
+    }
+    return false;
 };
+// Leap years are years divisible by four, NOT divisible by 100 UNLESS divisible by 400
 
 // Do not edit below this line
 module.exports = leapYears;
