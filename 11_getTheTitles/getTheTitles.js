c663d425f451e6aa1c7e39ecc1531c024136eb1f
Skubbed <167587317+Skubbed@users.noreply.github.com>
2024-07-26 15:27:54 -0700

    Finished 11 - getTheTitles. Will update after reading Solution.

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,6 +1,11 @@
-const getTheTitles = function() {
-
+  const getTheTitles = function() {
+    let bookList = [];
+    for(let i = 0; i < arguments[0].length; i++){
+        bookList[i] = arguments[0][i].title;
+    }
+    return bookList;
 };
 
 // Do not edit below this line
 module.exports = getTheTitles;
+
